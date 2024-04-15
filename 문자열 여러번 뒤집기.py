f763968482af84412d63813d181e27ba6c90d575
DESKTOP-R0PJKEE\wjg <wjg980807@gmail.com>
2024-04-15 23:04:25 +0900

    리스트reverse: [::-1]

--- 문자열 여러번 뒤집기.py
+++ 문자열 여러번 뒤집기.py
@@ -2,6 +2,7 @@ def solution(my_string, queries):
 
     for query in queries:
         my_string1= my_string[:query[0]]
+        #리스트 reverse: [::-1]
         my_string2= my_string[query[0]:query[1]+1][::-1]
         my_string3= my_string[query[1]+1:]
         my_string = my_string1+my_string2+my_string3
