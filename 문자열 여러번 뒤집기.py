70ea9d888536396175467e30992e6a2e95f53c06
DESKTOP-R0PJKEE\wjg <wjg980807@gmail.com>
2024-04-15 21:31:55 +0900

    2

---
+++ 문자열 여러번 뒤집기.py
@@ -0,0 +1,12 @@
+def solution(my_string, queries):
+
+    for query in queries:
+        my_string1= my_string[:query[0]]
+        my_string2= my_string[query[0]:query[1]+1][::-1]
+        my_string3= my_string[query[1]+1:]
+        my_string = my_string1+my_string2+my_string3
+
+
+    return my_string
+
+print(solution("rermgorpsam",[[2, 3], [0, 7], [5, 9], [6, 10]]))
